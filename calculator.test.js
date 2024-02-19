8939d325ed70cc148928d8ae7593d6678976996a
Jason <jasoncmcvicar@gmail.com>
2024-02-18 23:02:58 -0600

    working through some test cases, will need to revisit, since only one semi-working (not) working test is written, consult documentation, and also added use strict

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,4 @@
 it('should calculate monthly rate', function () {
-  // ...
+//  expect(calcForm.elements["loan-amount"].value).toEqual('10000');
+  expect(calcMonthlyPayment(10000, 10, 4.5)).toBe('103.64');
 });
